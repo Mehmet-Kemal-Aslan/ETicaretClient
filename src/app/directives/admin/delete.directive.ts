837acdca3a698efbb206a649b8c76378b8e54964
Mehmet-Kemal-Aslan <mehmetkemalaslan@gmail.com>
2023-09-11 22:18:40 +0300

    the sixteenth commit

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -1,6 +1,8 @@
 import { Directive, ElementRef, Renderer2, HostListener, Input, Output, EventEmitter } from '@angular/core';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
+import { MatDialog } from '@angular/material/dialog';
+import { DeleteDialogComponent, DeleteState } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
 
 declare var $: any;
 
@@ -12,7 +14,8 @@ export class DeleteDirective {
   constructor(
     private element: ElementRef,
     private _renderer: Renderer2,
-    private productService: ProductService) { 
+    private productService: ProductService,
+    public dialog: MatDialog) { 
       const img = _renderer.createElement("img")
       img.setAttribute("src", "../../../../../../assets/delete.png")
       img.setAttribute("style", "cursor: pointer")
@@ -26,11 +29,24 @@ export class DeleteDirective {
     @HostListener("click")
     async onclick()
     {
-      const td: HTMLTableElement = this.element.nativeElement;
-      await this.productService.delete(this.id);
-      $(td.parentElement).fadeOut(2000, () => {
-        this.callback.emit();
-      })
+      this.openDialog(async() =>
+      {
+        const td: HTMLTableElement = this.element.nativeElement;
+        await this.productService.delete(this.id);
+        $(td.parentElement).fadeOut(2000, () => {
+          this.callback.emit();
+        });
+      });
     }
 
+    openDialog(afterClosed: any): void {
+      const dialogRef = this.dialog.open(DeleteDialogComponent, {
+        data: DeleteState.Yes,
+      });
+  
+      dialogRef.afterClosed().subscribe(result => {
+        if(result == DeleteState.Yes)
+          afterClosed();
+      });
+    }
 }
