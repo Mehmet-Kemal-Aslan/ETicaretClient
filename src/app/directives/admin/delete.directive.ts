2b15579cf2346920290d2081bdcf00fbe5d4a6b9
Mehmet-Kemal-Aslan <mehmetkemalaslan@gmail.com>
2023-09-11 23:01:47 +0300

    the seventeenth commit

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -3,6 +3,8 @@ import { HttpClientService } from 'src/app/services/common/http-client.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteDialogComponent, DeleteState } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 declare var $: any;
 
@@ -14,8 +16,9 @@ export class DeleteDirective {
   constructor(
     private element: ElementRef,
     private _renderer: Renderer2,
-    private productService: ProductService,
-    public dialog: MatDialog) { 
+    private httpClientService: HttpClientService,
+    public dialog: MatDialog,
+    public alertifyService: AlertifyService) { 
       const img = _renderer.createElement("img")
       img.setAttribute("src", "../../../../../../assets/delete.png")
       img.setAttribute("style", "cursor: pointer")
@@ -25,6 +28,7 @@ export class DeleteDirective {
     }
 
     @Input() id: number
+    @Input() controller: string
     @Output() callback: EventEmitter<any> = new EventEmitter();
     @HostListener("click")
     async onclick()
@@ -32,9 +36,30 @@ export class DeleteDirective {
       this.openDialog(async() =>
       {
         const td: HTMLTableElement = this.element.nativeElement;
-        await this.productService.delete(this.id);
-        $(td.parentElement).fadeOut(2000, () => {
-          this.callback.emit();
+        //await this.productService.delete(this.id);
+        this.httpClientService.delete(
+          {
+            controller: this.controller
+          },this.id
+        ).subscribe(data => 
+          {
+            $(td.parentElement).fadeOut( () => {
+              this.callback.emit();
+              this.alertifyService.message("Ürün silinmiştir.",
+              {
+                dismissOthers: true,
+                messageType: MessageType.Success,
+                position: Position.TopRight
+              })
+          })
+        },(errorResponse: HttpErrorResponse) => 
+        {
+          this.alertifyService.message("Ürün silinememiştir.",
+          {
+            dismissOthers: true,
+            messageType: MessageType.Error,
+            position: Position.TopRight
+          })
         });
       });
     }
@@ -50,3 +75,7 @@ export class DeleteDirective {
       });
     }
 }
+
+
+
+
