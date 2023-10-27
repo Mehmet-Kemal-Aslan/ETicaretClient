dbe5f9d0ce6b4d45477404e94619b3bd0c4aec73
Mehmet-Kemal-Aslan <mehmetkemalaslan@gmail.com>
2023-10-28 01:45:44 +0300

    the twentysixth commit

--- src/app/ui/components/register/register.component.ts
+++ src/app/ui/components/register/register.component.ts
@@ -1,7 +1,12 @@
 import { group } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { User } from 'src/app/Entities/User';
+import { Create_User } from 'src/app/contracts/Users/Create_User';
+import { MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { UserService } from 'src/app/services/common/models/user.service';
+import { CustomToastrService, ToastrMessageType } from 'src/app/services/ui/custom-toastr.service';
 
 @Component({
   selector: 'app-register',
@@ -9,18 +14,20 @@ import { User } from 'src/app/Entities/User';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, 
+    private userService: UserService, 
+    private toastrService: CustomToastrService,) { }
 
   form: FormGroup;
   ngOnInit(): void
   {
     this.form = this.formBuilder.group({
-      adSoyad : ["", [
+      nameSurname : ["", [
         Validators.required,
         Validators.maxLength(50),
         Validators.minLength(3)
       ]],
-      kullaniciAdi : ["", [
+      userName : ["", [
         Validators.required,
         Validators.maxLength(50),
         Validators.minLength(3)
@@ -30,18 +37,18 @@ export class RegisterComponent {
         Validators.maxLength(70),
         Validators.email
       ]],
-      sifre : ["", [
+      password : ["", [
         Validators.required,
       ]],
-      sifreTekrar : ["", [
+      passwordConfirm : ["", [
         Validators.required,
       ]]
     },
     {
       validators: (group: AbstractControl): ValidationErrors | null => 
       {
-        let sifre = group.get("sifre").value;
-        let sifreTekrar = group.get("sifreTekrar").value;
+        let sifre = group.get("password").value;
+        let sifreTekrar = group.get("passwordConfirm").value;
         return sifre === sifreTekrar ? null : { notSame: true };
       }
     })
@@ -53,8 +60,28 @@ export class RegisterComponent {
   }
 
   submitted: boolean = false;
-  onSubmit(data: User)
+  async onSubmit(user: User)
   {
     this.submitted = true;
+    
+    if(this.form.invalid)
+      return
+
+    const result: Create_User = await this.userService.create(user);
+
+    if(result.succeeded)
+    {
+      console.log("Bura2" + result.message);
+      this.toastrService.message(result.message, 
+      ToastrMessageType.Success
+      )
+    }
+    else
+    {
+      console.log("Bura3" + result.message);
+      this.toastrService.message(result.message,
+          ToastrMessageType.Error
+          )
+    }
   }
 }
